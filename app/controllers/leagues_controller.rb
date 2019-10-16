733431d0db55df9ae632d7e5ed12fd9dfb020b97
rrankit <rankit.ranjan.contractor@move.com>
2019-10-17 02:31:21 +0530

    added routes and action to view  raw file

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -5,4 +5,8 @@ class LeaguesController < ApplicationController
   	@smallest_diff_team = get_smallest_diff_team(@leagues)
   end
 
+  def raw
+  	render file: Rails.root+"tmp/football.dat"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'leagues#index'
+  get 'raw', to: 'leagues#raw'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
